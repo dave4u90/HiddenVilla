e4ad9bc856b3c29c1c75696d0ea186746e1a2a3c
Debanjan Sengupta <dave4u90@gmail.com>
2021-07-11 21:37:09 +0530

    Initial Commit

---
+++ HiddenVilla_Server/Model/BlazorRoomProp.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace HiddenVilla_Server.Model
+{
+    public class BlazorRoomProp
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Value { get; set; }
+    }
+}
