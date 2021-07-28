28280b8e42d43fa99dd0456d239ce455db41ef88
Debanjan Sengupta <dave4u90@gmail.com>
2021-07-28 10:48:38 +0530

    handle redirect to login if user do not have access

---
+++ HiddenVilla_Client/Pages/Authentication/RedirectToLogin.razor.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Components;
+using Microsoft.AspNetCore.Components.Authorization;
+
+namespace HiddenVilla_Client.Pages.Authentication
+{
+    public partial class RedirectToLogin
+    {
+        public RedirectToLogin()
+        {
+        }
+
+        [Inject]
+        private NavigationManager navigationManager { get; set; }
+
+        [CascadingParameter]
+        private Task<AuthenticationState> authenticationState { get; set; }
+
+        bool notAuthorized { get; set; } = false;
+
+        protected override async Task OnInitializedAsync()
+        {
+            var authState = await authenticationState;
+            var returnUrl = navigationManager.ToBaseRelativePath(navigationManager.Uri);
+
+            if(authState?.User?.Identity is null || !authState.User.Identity.IsAuthenticated)
+            {
+                if (string.IsNullOrEmpty(returnUrl))
+                {
+                    navigationManager.NavigateTo("login", true);
+                }
+                else
+                {
+                    navigationManager.NavigateTo($"login?returnUrl={returnUrl}", true);
+                }
+            }
+            else
+            {
+                notAuthorized = true;
+            }
+            
+        }
+    }
+}
